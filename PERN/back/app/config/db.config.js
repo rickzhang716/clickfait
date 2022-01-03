7fd5a63048a27954cd5bf71cf9fd18044776c4e0
Rick Zhang <rz6zhang@uwaterloo.ca>
2022-01-03 12:32:15 -0800

    initial commit

---
+++ PERN/back/app/config/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "rickzhang",
+    PASSWORD: "password",
+    DB: "clickbait",
+    dialect: "postgres",
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
+
+};
---
+++ PERN/back/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// const { json } = require("stream/consumers");
+// const stream = require('stream');
+
+const app = express();
+
+var corsOptions = { origin: "http://localhost:8081" };
+
+app.use(cors(corsOptions));
+//setup express app
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+//require database
+const db = require("./app/models")
+db.sequelize.sync();
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+// });
+// //test home page
+// app.get("/", (req, res) => {
+//     res.send("Welcome to test PERN app.");
+// });
+
+require("./app/routes/tutorial.routes")(app);
+
+//set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}.`)
+})
